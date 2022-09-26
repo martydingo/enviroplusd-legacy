fae90f996d07c5d2d9b577c1050d17f9539ff829
marty@dingo.sh <marty@dingo.sh>
2022-09-26 12:40:21 +0100

    commit

--- enviroplusd/enviroplusd.py
+++ enviroplusd/enviroplusd.py
@@ -1,3 +1,30 @@
-import enviroplus.gas
+import bme280
+import ST7735
+from PIL import Image, ImageDraw, ImageFont
 
-print(enviroplus.gas.__dir__())
+
+class envplusd:
+    def __init__(self) -> None:
+        pass
+
+    def __initTemperatureSensor__(self) -> None:
+
+        self.TEMPERATURE_SENSOR = bme280.BME280()
+
+    def __initDisplay__(self) -> None:
+
+        self.DISPLAY = ST7735.ST7735(
+            port=0, cs=1, dc=9, backlight=12, rotation=270, spi_speed_hz=10000000
+        )
+        self.DISPLAY.begin()
+        self.DISPLAY_HEIGHT = self.DISPLAY.HEIGHT
+        self.DISPLAY_WIDTH = self.DISPLAY.WIDTH
+
+        initImg = Image.new(
+            mode="RGB",
+            size=(self.DISPLAY_HEIGHT, self.DISPLAY_WIDTH),
+            color=(33, 33, 33),
+        )
+        plac1 = Image
+        plac2 = ImageDraw
+        plac3 = ImageFont
